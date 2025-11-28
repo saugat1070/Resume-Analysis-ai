ddcbf0454187a174f005d1b8426e07596ef0c541
saugat1070 <saugatgiri1070@gmail.com>
2025-11-29 02:00:53 +0545

    adding careerpath profile recommnadation schema

--- src/Model/carrerpath.model.js
+++ src/Model/carrerpath.model.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
 const carrerSchema = new mongoose.Schema({
-    
+    careerName:{
+        type: String
+    },
+    description: String,
+    requireSkils: [String],
+    marketDemands: [String]
 })
+
+const Career = mongoose.model("Career",carrerSchema);
+export {Career};
