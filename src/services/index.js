5fd9f58d114ee80b757cad778ffd1da81385eb46
saugat1070 <saugatgiri1070@gmail.com>
2025-12-24 22:10:27 +0545

    Inngest handling point

--- src/services/index.js
+++ src/services/index.js
@@ -1,17 +1,17 @@
 import { serve } from "inngest/express";
 import { app } from "../main.js";
 import { inngest } from "./inngest/client.js";
-import {singleStage} from "./inngest/functions/function.js"
+import {AiResponse} from "./inngest/functions/function.js"
 
 app.use("/api/inngest",serve({
     client: inngest,
-    functions: [singleStage]
+    functions: [AiResponse]
 }));
 
 
-app.get("/api/hello",async(req,res)=>{
-    await inngest.send({
-        name:"test/event.first"
-    });
-    return res.json({message:"hello-world"})
-})
+// app.get("/api/hello",async(req,res)=>{
+//     const {ids} = await inngest.send({
+//         name:"test/resume.ranking"
+//     });
+//     return res.json({message:"hello-world",event:ids})
+// })
