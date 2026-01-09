a933889857681fe741aa9a450b21f88db768c555
saugat1070 <saugatgiri1070@gmail.com>
2026-01-09 18:46:04 +0545

    Add isVerified

--- src/Model/user.model.js
+++ src/Model/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -33,6 +34,10 @@ const userSchema = new mongoose.Schema({
     DateCreated:{
         type: Date,
         default: Date.now()
+    },
+    isVerified : {
+        type : Boolean,
+        default: false
     }
 });
 
