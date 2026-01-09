3c31e19bdd7faf32b4b49965d74a65dc82b7757f
saugat1070 <saugatgiri1070@gmail.com>
2026-01-09 18:45:39 +0545

    small change on generateToken

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,8 @@
 import jwt from "jsonwebtoken";
 import { envConfig } from "../Config/envConfig.js";
 
-export const generateToken = (data,res)=>{
-    console.log(data)
-    const token = jwt.sign({_id:data?._id},envConfig?.jwtSecretToken,{expiresIn:"3d"});
+export const generateToken = (token,res)=>{
+    // const token = jwt.sign({_id:data?._id},envConfig?.jwtSecretToken,{expiresIn:"3d"});
     res.cookie("jwt",token,{
         maxAge:24*60*60*1000,
         httpOnly: true,
